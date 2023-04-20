4b648fa61d3d724993e522bb19b618814bfc4330
a.parvaneh <a.parvaneh@middleeastbank.ir>
2023-04-20 09:33:24 +0330

    dev-users-api

--- src/main/java/org/quera/ticket/controller/UserController.java
+++ src/main/java/org/quera/ticket/controller/UserController.java
@@ -3,9 +3,11 @@ package org.quera.ticket.controller;
 import org.quera.ticket.models.User;
 import org.quera.ticket.security.services.UserServiceImpl;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
 
 @RestController
@@ -18,14 +20,21 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping(value = "/all-users")
+    public ResponseEntity<List<User>> getAllUser() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping(value = "/{id}/update_balance")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestParam BigDecimal balance) {
         User user = userService.findUserById(id);
-        if (Objects.nonNull(user)){
+        if (Objects.nonNull(user)) {
             user.setBalance(balance);
             userService.saveUser(user);
             return ResponseEntity.ok(user);
-        }else
+        } else
             return null;
     }
 }
