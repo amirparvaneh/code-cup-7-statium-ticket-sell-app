f6550acb94647fc7f1aed1343fdbd1b56ae9824e
a.parvaneh <a.parvaneh@middleeastbank.ir>
2023-04-26 11:45:41 +0330

    dev-stadium-api

--- src/main/java/org/quera/ticket/controller/StadiumController.java
+++ src/main/java/org/quera/ticket/controller/StadiumController.java
@@ -4,14 +4,13 @@ import org.quera.ticket.dto.request.StadiumRequestDto;
 import org.quera.ticket.dto.response.StadiumResponseDto;
 import org.quera.ticket.models.Stadium;
 import org.quera.ticket.repository.UserRepository;
+import org.quera.ticket.security.services.UserDetailsImpl;
 import org.quera.ticket.service.StadiumServiceImpl;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.Authenticator;
+
 import java.util.List;
 
 @RestController
@@ -39,12 +38,22 @@ public class StadiumController {
     public ResponseEntity<StadiumResponseDto> addNewStadium(@RequestBody StadiumRequestDto stadiumRequestDto,
                                                             Authentication authentication){
 
+        Long userId = ((UserDetailsImpl) authentication.getPrincipal()).getUser().getId();
+        StadiumResponseDto stadiumResponseDto = new StadiumResponseDto();
         try{
             if (stadiumRequestDto.getCapacity() < 0){
-                StadiumResponseDto stadiumResponseDto = new StadiumResponseDto();
                 stadiumResponseDto.setMessage("Error: invalid balance");
                 return ResponseEntity.badRequest().body(stadiumResponseDto);
-            }else if (!userRepository.existsById())
+            }else if (!userRepository.existsById(userId)){
+                stadiumResponseDto.setMessage("Error: user not found");
+            }else {
+                stadiumService.addNewStadium(stadiumRequestDto);
+                stadiumResponseDto.setMessage("saved");
+                return ResponseEntity.ok(stadiumResponseDto);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
+        return ResponseEntity.ok(stadiumResponseDto);
     }
 }
